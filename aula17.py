fac6ae3406c50dfc87c787845d7473b1413ded40
Ângela S. M. Cunha <angelassilviane@gmail.com>
2025-01-14 20:08:51 -0300

    Update code 14

---
+++ aula17.py
@@ -0,0 +1,9 @@
+num1 = input('Digite o número 1: ')
+num2 = input('Digite o número 2: ')
+
+if num1 > num2:
+    print(f'{num1} é maior que {num2}')
+elif num1 == num2:
+    print(f'{num1} é igual a {num2}')
+else:
+    print(f'{num2} é mmaior que {num1}')
