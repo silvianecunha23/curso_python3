6fb6c9c6dc664f79f53f3ac68f4d62cc1529704b
Ângela S. M. Cunha <angelassilviane@gmail.com>
2025-01-27 18:12:14 -0300

    Update code 29

--- aula29.py
+++ aula29.py
@@ -21,3 +21,17 @@ try:
 except:
     # Se deixar espaço em branco, digitar caracteres ou qualquer outra coisa:
     print('Isso não é um número!')
+
+########################################################
+# Segunda opção de solução:
+# entrada = input('Digite um número: ')
+
+# if entrada.isdigit():
+#     entrada_int = int(entrada)
+#     par_impar = entrada_int % 2 == 0
+#     par_impar_texto = 'ímpar'
+#     if par_impar:
+#         par_impar_texto = 'par'
+#     print(f'O número {entrada_int} é {par_impar_texto}')
+# else:
+#     print('Você não digitou um número inteiro')
